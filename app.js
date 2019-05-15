1798227fa2fe15e9bffbcf70e8521536e4c80f8d
Maty <matroxmega@gmail.com>
2019-05-15 19:20:47 -0400

    another commit doesnt matter

--- app.js
+++ app.js
@@ -6,7 +6,13 @@ var express= require('express');
 var app = express();
 
 //escuchar peticiones
-
 app.listen(3000, ()=>{
     console.log('Express Server - puerto 3000 online');
 });
+
+app.get('/',(req,res, next)=>{
+    res.status(200).json({
+        ok: true,
+        mensaje: 'Petición realizada correctamente'
+    })
+} );
