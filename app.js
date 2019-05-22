ac52f73c4e9566812976fc74b52a02ec545d6e13
Maty <matroxmega@gmail.com>
2019-05-22 18:45:10 -0400

    se realizo la conexion con una bdd y se agregaron metodos get y post

--- app.js
+++ app.js
@@ -1,13 +1,57 @@
 //requires
 var express= require('express');
-
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
 //inicializar variables
-
 var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+//configurar conexion
+const mc= mysql.createConnection({
+    host: 'localhost',
+    user:'root',
+    password:'',
+    database:'angular'
+});
+
+mc.connect();
+
 
 //escuchar peticiones
 app.listen(3000, ()=>{
     console.log('Express Server - puerto 3000 online');
+});
+
+app.post('/producto', function(req, res){
+    let datosProducto ={
+        productName: req.body.name,
+        productCode: req.body.code,
+        releaseDate: req.body.date,
+        price: parseInt(req.body.price),
+        description: req.body.description,
+        starRating: parseInt(req.body.rating),
+        imageUrl: req.body.image
+    };
+    if(mc){
+        mc.query("INSERT INTO productos set ?",datosProducto, function (error,result){
+            if(error){
+                res.status(500).json({"Mensaje": "Error"});
+            }else{
+                res.status(201).json({"Mensaje": "Insertado"});
+            }
+        });
+    }
+});
+
+app.get('/productos',function(req,res){
+    mc.query('SELECT * FROM productos', function(error, results, fields){
+        if(error) throw error;
+        return res.send({
+            error: false, data: results, message:'Lista de productos.'
+        });
+    });
 });
 
 app.get('/',(req,res, next)=>{
