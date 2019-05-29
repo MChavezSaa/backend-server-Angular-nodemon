16627f0c597d90b4c8344861185f5b279af16076
maty <matroxmega@gmail.com>
2019-05-29 09:50:16 -0400

    small changes idk what im doing

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(function(req, res,next){
     res.header("Access-Control-Allow-Origin","*");
     res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
     res.header("Access-Control-Allow-Headers",
-    "Origin,X-Requested-With, Content-Type,Accept, x-cliente-key, x-cliente-token, x-client-secret, Authorization");
+    "Origin, X-Requested-With, Content-Type, Accept, x-cliente-key, x-cliente-token, x-client-secret, Authorization");
     next();
 });
 
